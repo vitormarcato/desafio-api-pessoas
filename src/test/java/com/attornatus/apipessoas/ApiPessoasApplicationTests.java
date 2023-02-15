9dec899a916e452c4ca89f8326fb37b311689a4c
Vitor Marcato <vitor.marcatof@gmail.com>
2023-02-15 20:19:57 -0300

    first commit: projeto criado
    criado com Sprig Initializr, utilizando Spring Boot 3, Maven, Java 17

---
+++ src/test/java/com/attornatus/apipessoas/ApiPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.apipessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
