3119ba2d8e663a8e41befb92c99d5353ab9d705a
Vitor Marcato <vitor.marcatof@gmail.com>
2023-02-18 17:36:39 -0300

    feat(atribuir Endereco Para Pessoa) adicionado mcamada services + implementado atribuir endereco para pessoa
    Camada service criada com lógica da funcionalidade atribuir endereco para pessoa. Nova funcionalidade testada via postman

--- src/main/java/com/attornatus/apipessoas/controllers/PessoaController.java
+++ src/main/java/com/attornatus/apipessoas/controllers/PessoaController.java
@@ -4,13 +4,11 @@ import com.attornatus.apipessoas.models.DadosCadastroPessoa;
 import com.attornatus.apipessoas.models.DadosDetalhamentoPessoa;
 import com.attornatus.apipessoas.models.Pessoa;
 import com.attornatus.apipessoas.repositories.PessoaRepository;
+import com.attornatus.apipessoas.services.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -18,6 +16,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class PessoaController {
     @Autowired
     private PessoaRepository repository;
+    @Autowired
+    private PessoaService service;
 
     @PostMapping
     @Transactional
@@ -28,6 +28,13 @@ public class PessoaController {
         var uri = uriBuilder.path("/pessoas/{id}").buildAndExpand(pessoa.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DadosDetalhamentoPessoa(pessoa));
+    }
+
+    @PutMapping("/{pessoaId}/enderecos/{enderecoId}")
+    @Transactional
+    public ResponseEntity atribuirEnderecoParaPessoa(@PathVariable Long pessoaId, @PathVariable Long enderecoId){
+        var pessoa = service.atribuirEnderecoParaPessoa(pessoaId, enderecoId);
+        return ResponseEntity.ok(new DadosDetalhamentoPessoa(pessoa));
     }
 
 }
---
+++ src/main/java/com/attornatus/apipessoas/services/PessoaService.java
@@ -0,0 +1,33 @@
+package com.attornatus.apipessoas.services;
+
+import com.attornatus.apipessoas.models.Endereco;
+import com.attornatus.apipessoas.models.Pessoa;
+import com.attornatus.apipessoas.repositories.EnderecoRepository;
+import com.attornatus.apipessoas.repositories.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    public Pessoa atribuirEnderecoParaPessoa(Long pessoaId, Long enderecoId) {
+        Pessoa pessoa = pessoaRepository.findById(pessoaId).get();
+        Endereco endereco = enderecoRepository.findById(enderecoId).get();
+        pessoa.getEnderecos().add(endereco);
+        endereco.getPessoasNoEndereco().add(pessoa);
+
+        //Set<Endereco> enderecosSet = pessoa.getEnderecos();
+        //enderecosSet.add(endereco);
+        return pessoaRepository.save(pessoa);
+    }
+
+
+}
